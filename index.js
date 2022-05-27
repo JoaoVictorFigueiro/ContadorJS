a07baeed6c011ea181c4f60f14586f9cd88187db
João Figueiró <joviooli@gmail.com>
2022-05-27 09:57:59 -0300

    Finalizacao_projeto

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
+var adicionarValor = document.getElementById("adicionar");
+var resetarValor = document.getElementById("resetar");
+var retirarValor = document.getElementById("retirar")
 var numerais = document.getElementById("contador");
 
 var numeros = 0;
 
 numerais.innerHTML = numeros;
 
-// function adicionar() {
+adicionarValor.addEventListener('click', function(){
+    numerais.innerHTML = ++numeros;
+});
 
-// }
+resetarValor.addEventListener('click', function(){
+    numerais.innerHTML = numeros = 0;
+})
 
-// function remover() {
-
-// }
-
-// function resetar() {
-
-// }
+retirarValor.addEventListener('click', function(){
+    numerais.innerHTML = -- numeros;
+})
