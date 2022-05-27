b3899d5b110bcc2ba70f08f861f6c3eac5348dee
João Figueiró <joviooli@gmail.com>
2022-05-27 09:12:29 -0300

    Atualizacao_dados

---
+++ index.js
@@ -0,0 +1,17 @@
+var numerais = document.getElementById("contador");
+
+var numeros = 0;
+
+numerais.innerHTML = numeros;
+
+// function adicionar() {
+
+// }
+
+// function remover() {
+
+// }
+
+// function resetar() {
+
+// }
